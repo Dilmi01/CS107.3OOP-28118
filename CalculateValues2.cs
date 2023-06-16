e825c0df46edfd4d351081c33dd102ac4f0a9c72
Dilmi <118357973+demi2k1@users.noreply.github.com>
2023-06-16 14:31:56 +0530

    Add files via upload

---
+++ CalculateValues2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q3t6.Properties
+{
+    internal class CalculateValues
+    {
+        public double sum(double num1, double num2) 
+        {
+            return num1 + num2;
+
+        }
+        public double sub(double num1, double num2)
+        {
+            return num1 - num2;
+
+        }
+        public double mul(double num1, double num2)
+        {
+            return num1 * num2;
+
+        }
+        public double div(double num1, double num2)
+        {
+            return num1 / num2;
+
+        }
+    }
+}
---
+++ q3t06.cs
@@ -0,0 +1,55 @@
+﻿using q3t6.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace q3t6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 1 for Addition");
+            Console.WriteLine("Enter 2 for subtraction");
+            Console.WriteLine("Enter 3 for Multiplication");
+            Console.WriteLine("Enter 4 for Division");
+
+            Console.WriteLine("Enter your choice:");
+            Console.ReadKey();
+
+            string input = Console.ReadLine();
+
+            Console.WriteLine("\n Enter number 1 :") ;
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n Enter number 2 :");
+            double num2 = double.Parse(Console.ReadLine());
+
+            CalculateValues obj= new CalculateValues();
+
+            double answer = 0;
+
+            if (input == "1")
+            {
+                answer = obj.sum(num1,num2);
+            }
+            else if (input == "2")
+            {
+                answer = obj.sum(num1, num2);
+
+            }
+            else if (input == "3")
+            {
+                answer = obj.sum(num1, num2);
+            }
+            else if (input == "4")
+            {
+                answer = obj.sum(num1, num2);
+            }
+            Console.WriteLine("Answer is: " + answer);
+
+        }
+    }
+}
