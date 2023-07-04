6e886422c3d1c856524fc6db7cf16f1836c14fe3
Dilmi01 <118357973+Dilmi01@users.noreply.github.com>
2023-07-04 14:23:01 +0530

    Add files via upload

---
+++ tute5/tuto5 q1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tutorials5.Properties;
+
+namespace tutorials5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter km value");
+            double km = double.Parse(Console.ReadLine());
+
+            ConvertValues obj = new ConvertValues();
+            obj.kilometerTOmeter(km);
+
+
+            Console.ReadLine();
+        }
+    }
+}
