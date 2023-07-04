9a37733d404dd7048b37d795af5cbe008d6b796f
Dilmi01 <118357973+Dilmi01@users.noreply.github.com>
2023-07-04 14:21:56 +0530

    Add files via upload

---
+++ tute3/tute3 q2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tute3_q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter input1= ");
+            string num1=Console.ReadLine();
+            Console.WriteLine("Enter input2= ");
+            string num2 = Console.ReadLine();
+            
+            double num1= double.Parse(num1);
+            double num2 = double.Parse(num2);   
+
+            Console.WriteLine(num1  "+" + num2   "=" + (num1+num2));
+            Console.ReadLine();
+
+
+        }
+        
+    }
+}
