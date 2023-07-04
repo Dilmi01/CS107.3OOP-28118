dd5542746f03d0b183721d45e1c1e69362519841
Dilmi01 <118357973+Dilmi01@users.noreply.github.com>
2023-07-04 14:23:39 +0530

    Add files via upload

---
+++ tute7/t7q1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tute7q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Classt7 objhello = new Classt7();
+            objhello.sayHello();
+
+            Console.ReadLine();
+        }
+    }
+}
