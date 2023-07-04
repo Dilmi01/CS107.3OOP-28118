fc1ef6456bf07a39c55632dba8424993d66d741f
Dilmi01 <118357973+Dilmi01@users.noreply.github.com>
2023-07-04 14:24:04 +0530

    Add files via upload

---
+++ tute6/FindValues.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tute08q2
+{
+    internal class FindValues
+    {
+        public double findArea(double r)
+        {
+           //Console.WriteLine("Enter the radius value:");
+            //double r = double.Parse(Console.ReadLine());
+
+            double A = 3.142 * r * r;
+            return A;
+        }
+
+        public double findCircumference(double r) 
+        {
+           // Console.WriteLine("Enter the radius value:");
+            //double r = double.Parse(Console.ReadLine());
+            double C = 2 * 3.142 * r;
+            return C;   
+        }
+    }
+}
---
+++ tute6/tute6 q2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tute08q2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the radius value:");
+            double r = double.Parse(Console.ReadLine());
+
+          
+            FindValues objFindValues = new FindValues();
+            double A = objFindValues.findArea(r);
+
+            Console.WriteLine("Area = " + A);
+
+            
+            double C = objFindValues.findCircumference(r);
+            Console.WriteLine("Circumference = " + C);
+            Console.ReadLine();
+        }
+    }
+}
