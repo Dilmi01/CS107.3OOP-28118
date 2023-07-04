e1ddfc68ad28e734efd268f801d431d2c397aa3b
Dilmi01 <118357973+Dilmi01@users.noreply.github.com>
2023-07-04 14:22:16 +0530

    Add files via upload

---
+++ tute4/arrays.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_4_new_q_arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a[] = string[3] { 0, 1, 2 };
+            string b[] =  string[3] { 0, 1, 2 };
+             
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ tute4/tute4 q1 ConvertkilometerTO meter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tute4_q1
+{
+    public class ConvertValues
+    {
+          public void kilometerTOmeter ()
+          {
+            
+          }
+
+                 
+
+
+    }
+}
