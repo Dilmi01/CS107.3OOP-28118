f8d5da55c1b25e8cfaf8a2e5fe42845358b24136
Dilmi01 <118357973+Dilmi01@users.noreply.github.com>
2023-07-04 14:20:22 +0530

    Add files via upload

---
+++ tute1/tute1.cs
@@ -0,0 +1,44 @@
+﻿  
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_1_c_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name : ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hi!"+name);
+            int mark = 50;
+            if(mark >=75)
+            {
+                Console.WriteLine("Grade A");
+            }else if(mark >=65 && mark<75)
+            {
+                Console.WriteLine("Grade B");
+            }
+            else if (mark >= 55 && mark < 65)
+            {
+                Console.WriteLine("Grade C");
+            }
+            else if (mark >= 45 && mark < 55)
+            {
+                Console.WriteLine("Grade C");
+            }
+            else if (mark >= 35 && mark < 45)
+            {
+                Console.WriteLine("Grade S");
+            }
+            else if ( mark < 35)
+            {
+                Console.WriteLine("Fail");
+            }
+            Console.ReadLine();
+        }
+    }
+}
