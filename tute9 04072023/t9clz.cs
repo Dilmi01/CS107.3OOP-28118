9e69d5b7c4ad887a19787c21744b02cba564da1b
Dilmi01 <118357973+Dilmi01@users.noreply.github.com>
2023-07-04 11:03:28 +0530

    Add files via upload

---
+++ tute9 04072023/t9clz.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tute_9_q1.Properties
+{
+    internal class t9clz
+    {    
+        public int[] arrayMethod(int size)
+        { 
+            int[] array = new int[size];
+
+            for (int i = 0; i < array.Length; i += 2)
+            {
+                Console.WriteLine("Enter a value :" );
+                int value = Convert.ToInt32(Console.ReadLine());
+
+                array[i] = value;
+                array[i + 1] = 0;
+            }
+            return array;
+
+        }
+      
+
+
+    }
+}
---
+++ tute9 04072023/t9q1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tute_9_q1.Properties;
+
+namespace tute_9_q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size of the array:");
+            int size  = Convert.ToInt32(Console.ReadLine());
+
+            t9clz obj1= new t9clz();
+            int[] array = obj1.arrayMethod(size);
+
+            Console.WriteLine("\nDisplay the array:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i] + "");            
+            }
+            Console.ReadLine();
+        }
+    }
+}
